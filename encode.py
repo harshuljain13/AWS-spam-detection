f143befbb0b3726639a2a83908029958520d2690
Harshul jain <harshuljain1393@gmail.com>
2020-05-15 13:12:29 -0400

    Create encode.py

---
+++ encode.py
@@ -0,0 +1,77 @@
+import string
+import sys
+import numpy as np
+
+from hashlib import md5
+
+if sys.version_info < (3,):
+    maketrans = string.maketrans
+else:
+    maketrans = str.maketrans
+
+def vectorize_sequences(sequences, vocabulary_length):
+    results = np.zeros((len(sequences), vocabulary_length))
+    for i, sequence in enumerate(sequences):
+       results[i, sequence] = 1. 
+    return results
+
+def one_hot_encode(messages, vocabulary_length):
+    data = []
+    for msg in messages:
+        temp = one_hot(msg, vocabulary_length)
+        data.append(temp)
+    return data
+
+def text_to_word_sequence(text,
+                          filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n',
+                          lower=True, split=" "):
+    
+    if lower:
+        text = text.lower()
+
+    if sys.version_info < (3,):
+        if isinstance(text, unicode):
+            translate_map = dict((ord(c), unicode(split)) for c in filters)
+            text = text.translate(translate_map)
+        elif len(split) == 1:
+            translate_map = maketrans(filters, split * len(filters))
+            text = text.translate(translate_map)
+        else:
+            for c in filters:
+                text = text.replace(c, split)
+    else:
+        translate_dict = dict((c, split) for c in filters)
+        translate_map = maketrans(translate_dict)
+        text = text.translate(translate_map)
+
+    seq = text.split(split)
+    return [i for i in seq if i]
+
+def one_hot(text, n,
+            filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n',
+            lower=True,
+            split=' '):
+   
+    return hashing_trick(text, n,
+                         hash_function='md5',
+                         filters=filters,
+                         lower=lower,
+                         split=split)
+
+
+def hashing_trick(text, n,
+                  hash_function=None,
+                  filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n',
+                  lower=True,
+                  split=' '):
+    
+    if hash_function is None:
+        hash_function = hash
+    elif hash_function == 'md5':
+        hash_function = lambda w: int(md5(w.encode()).hexdigest(), 16)
+
+    seq = text_to_word_sequence(text,
+                                filters=filters,
+                                lower=lower,
+                                split=split)
+    return [int(hash_function(w) % (n - 1) + 1) for w in seq]
